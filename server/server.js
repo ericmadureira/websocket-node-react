54247504b20cb91e7ef2f8d7d3158a06b095a849
Eric Madureira <ericmadu416@gmail.com>
2024-03-03 01:35:44 -0300

    feat: enable cors

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const { createServer } = require('node:http')
-const { join } = require('node:path')
+const cors = require('cors')
 const { Server } = require('socket.io')
 
 const app = express()
-const server = createServer(app)
-const io = new Server(server)
+app.use(cors())
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+  }
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'))
+  res.send('hello from node server')
 })
 
 io.on('connection', (socket) => {
@@ -18,6 +23,6 @@ io.on('connection', (socket) => {
   });
 })
 
-server.listen(3434, () => {
+httpServer.listen(3434, () => {
   console.log('Server running at http://localhost:3434')
 })
