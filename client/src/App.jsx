2727b3e4c9fdb577e5afe155d8bcdaac86697957
Eric Madureira <ericmadu416@gmail.com>
2024-03-03 17:39:42 -0300

    feat: display received messages in chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 
 const socket = io.connect("http://localhost:3434")
 
 const App = () => {
   const [messageInput, setMessageInput] = useState('')
+  const [receivedMessages, setReceivedMessages] = useState([])
 
   const sendMessage = (event) => {
     event.preventDefault()
@@ -12,10 +13,22 @@ const App = () => {
     setMessageInput('')
   }
 
+  useEffect(() => {
+    // Receive message EVENT
+    socket.on('receive_message', (data) => {
+      // Update list of received messages
+      setReceivedMessages([...receivedMessages, data])
+    })
+  }, [receivedMessages])
+
   return (
     <div>
       <h1>Socket.io Chat</h1>
-      <ul id="messages"></ul>
+
+      <ul id="messages">
+        {receivedMessages.map(message => <li key={message.timestamp}>{message.content}</li>)}
+      </ul>
+
       <form id="form" action="">
         <input id='input' autoComplete="off" value={messageInput} onChange={(event) => setMessageInput(event.target.value)} />
         <button onClick={sendMessage}>Send</button>
